3a6b0305618aed8f8eea811608db6f15cad6c7c1
cshah <cshah@tavisca.com>
2017-03-05 22:55:37 +0530

    Initial Commit

---
+++ SwapApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fristNumber, secondNumber;
+            Console.WriteLine("Enter the Frist Number");
+            fristNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numbers you have Entered are " + fristNumber + " , " + secondNumber);
+            Console.WriteLine("Press any key to swap the number");
+            Console.ReadKey();
+            SwapNumberManager swapObject = new SwapNumberManager();
+            swapObject.SwapFunction(fristNumber,secondNumber);
+
+
+
+                
+        }
+    }
+}
---
+++ SwapApplication/Swap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapApplication
+{
+    class SwapNumberManager
+    {
+        public void SwapFunction(int firstNumber, int secondNumber)
+        {
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+            Console.WriteLine("Numbers after Swap are : \n");
+            Console.WriteLine("FristNumber is = " + firstNumber + " SecondNumber is = " + secondNumber);
+            Console.WriteLine("Thankyou\n\nPress Any key to Exit the Application...");
+            Console.ReadKey();
+        }
+
+    }
+}
